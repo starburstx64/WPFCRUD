b2120e00befd2e0280255754f62d29e7485721ba
David Segovia Ley <starburstx64@gmail.com>
2019-02-17 15:34:59 -0600

    Agregar archivos de proyecto.

---
+++ WpfDataBaseBOY/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WpfDataBaseBOY
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities db = new NorthwindEntities ();
+        public  static DataGrid datag;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        private void Load()
+        {
+            DataGrid1.ItemsSource = db.Categories.ToList();
+            datag = DataGrid1;
+        }
+
+        private void UpdateBTN_Click(object sender, RoutedEventArgs e)
+        {
+            int categoryId = (DataGrid1.SelectedItem as Categories).CategoryID;
+            WindowUpdate Upage = new WindowUpdate(categoryId);
+            Upage.ShowDialog();
+        }
+        
+
+    }
+}
---
+++ WpfDataBaseBOY/WindowUpdate.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDataBaseBOY
+{
+    /// <summary>
+    /// Interaction logic for WindowUpdate.xaml
+    /// </summary>
+    public partial class WindowUpdate : Window
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        int id;
+
+        public WindowUpdate(int CategoryID)
+        {
+            InitializeComponent();
+            id = CategoryID;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Categories updateCat = (from m in db.Categories where m.CategoryID == id select m).Single();
+            updateCat.CategoryName = CatNameText.Text;
+            updateCat.Description = DescText.Text;
+            db.SaveChanges();
+            MainWindow.datag.ItemsSource = db.Categories.ToList();
+            this.Hide();
+
+        }
+    }
+}
